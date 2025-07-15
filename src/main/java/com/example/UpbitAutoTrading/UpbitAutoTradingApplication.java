b902f5dae147d26e7b76b878dde7b7f3536bc9ba
hanmingi <hk335078@naver.com>
2025-07-15 13:20:49 +0900

    [ADD] First Commit

---
+++ src/main/java/com/example/UpbitAutoTrading/UpbitAutoTradingApplication.java
@@ -0,0 +1,13 @@
+package com.example.UpbitAutoTrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbitAutoTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbitAutoTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UpbitAutoTrading/UpbitAutoTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UpbitAutoTrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpbitAutoTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
